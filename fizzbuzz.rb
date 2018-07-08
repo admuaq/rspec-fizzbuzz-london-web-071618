4a6a54966b2e4f455fe3b702f9b410eadfc9fd12
Adham Muhammad <admuaq@gmail.com>
2018-07-08 05:26:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,20 @@
-#Define method fizzbuzz
 
-def fizzbuzz
+
+def fizzbuzz (int)
+  #We expect fizzbuzz(15) to return "Fizzbuzz"
+  elsif int % 3 == 0 && int % 5 == 0
+    "Fizzbuzz"
   
+  #We expect fizzbuzz(3) to return "Fizz"
+  if int % 3 == 0
+    "Fizz"
+  
+  #We expect fizzbuzz(4) to return nil.
+  if int % 3 != 0 || int % 5 !=0
+    return
 end
 
-#We expect fizzbuzz(3) to return "Fizz"
 #We expect fizzbuzz(5) to return "Buzz"
+
 #We expect fizzbuzz(15) to return "FizzBuzz"
 #We expect fizzbuzz(4) to return nil.
