641de9ebec8161113c118f4ba5f6450ea6d54572
Adham Muhammad <admuaq@gmail.com>
2018-07-08 05:29:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,15 +4,18 @@ def fizzbuzz (int)
   #We expect fizzbuzz(15) to return "Fizzbuzz"
   if int % 3 == 0 && int % 5 == 0
     "Fizzbuzz"
+    
+  end
   
   #We expect fizzbuzz(3) to return "Fizz"
   elsif int % 3 == 0
     "Fizz"
+  end
   
   #We expect fizzbuzz(4) to return nil.
   else int % 3 != 0 || int % 5 !=0
     return
-    
+  end
 end
 
 #We expect fizzbuzz(5) to return "Buzz"
